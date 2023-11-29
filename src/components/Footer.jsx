d29b46f5dfe3018ee27e5b167291c022b6eb5bed
nil168 <swapnanil52@gmail.com>
2023-11-30 00:14:48 +0530

    social media links added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,13 @@ import {
   AiFillLinkedin,
   AiOutlineArrowUp,
 } from 'react-icons/ai';
-import avatar from '../assets/avatar.jpg'
+import avatar from '../assets/avatar.jpg';
 
 const Footer = () => {
   return (
     <footer>
       <div>
-        <img
-          src={avatar}
-          alt="Founder"
-        />
+        <img src={avatar} alt="Founder" />
         <h2>Swapnanil mandal</h2>
         <p>
           Life is tragedy in short shots, but its a comedy in a long
@@ -24,13 +21,13 @@ const Footer = () => {
       <aside>
         <h2>Social Media</h2>
         <article>
-          <a href="https://www.youtube.com/">
+          <a href="https://www.linkedin.com/in/swapnanilmandal/">
             <AiFillLinkedin />
           </a>
-          <a href="https://www.youtube.com/">
+          <a href="https://www.instagram.com/">
             <AiFillInstagram />
           </a>
-          <a href="https://www.youtube.com/">
+          <a href="https://www.facebook.com/me.swapnanil.mandal">
             <AiFillFacebook />
           </a>
         </article>
